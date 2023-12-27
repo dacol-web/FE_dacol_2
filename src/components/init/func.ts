44620fcd6ac6452ecdd4c4bad43e4b9539e72b9c
Hy-Iam-Noval <upangwoy@gmail.com>
2023-12-27 21:54:58 +0700

    s

--- src/components/init/func.ts
+++ src/components/init/func.ts
@@ -33,7 +33,7 @@ export type Error<T> = {errors:T}
 export type LoadingCtx = [boolean, React.Dispatch<React.SetStateAction<boolean>>]
 
 export const 
-   API = axios.create({baseURL:"http://localhost:8080"}),
+   API = axios.create({baseURL:process.env.REACT_APP_API_BASE}),
 
    getUser = () => localStorage.getItem(USERKEY),
    parseToken = () => {
